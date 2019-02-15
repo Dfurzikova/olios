4b4e7119e3ef47ceea200b7eedd0c9756937abf3
Dfurzikova <dfurzikova@yandex.ru>
2019-02-15 17:04:36 +0300

    feat: add animation

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import { cn } from '@bem-react/classname';
 import './Hamburger.scss';
+import Menu from '../Menu/Menu'
 const hamburger = cn('Hamburger');
 
 class Hamburger extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            menuActive: false
+        }
+        this.gamburgerClick = this.gamburgerClick.bind(this)
+    }
+    gamburgerClick(){
+        if (this.state.menuActive) {
+
+            this.state.className = ""
+
+        } else {
+            this.state.className = "active"
+        }
+
+        this.setState({
+            menuActive: !this.state.menuActive,
+        })
+
+    }
+
+    renderMenu(){
+        if(this.state.menuActive){
+            return (
+                <Menu/>
+            )
+        }
+    }
+
     render() {
         return (
-            <div className={hamburger()}>
+            <React.Fragment>
+            <div className={hamburger('', [this.state.className])}  onClick={this.gamburgerClick} >
+
                <ul className={hamburger('Lines')}>
-                   <li className={hamburger('Line')}></li>
-                   <li className={hamburger('Line')}></li>
-                   <li className={hamburger('Line')}></li>
+                   <li className={hamburger('Line', ['Hamburger-Line_1'])}></li>
+                   <li className={hamburger('Line', ['Hamburger-Line_2'])}></li>
+                   <li className={hamburger('Line', ['Hamburger-Line_3'])}></li>
                </ul>
             </div>
+            {this.renderMenu()}
+            </React.Fragment>
         )
     }
 }

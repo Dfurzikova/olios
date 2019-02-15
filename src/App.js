4e016698e51cd3bdb933f4f89bfa833141d632ca
Dfurzikova <dfurzikova@yandex.ru>
2019-02-15 18:55:03 +0300

    feat: add menu routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Cart from './components/Cart/Cart';
 import Search from './components/Search/Search'
 import Hamburger from './components/Hamburger/Hamburger';
+import Content from './components/Content/Content'
 
 class App extends Component {
     render() {
@@ -17,6 +18,11 @@ class App extends Component {
                         <Route path="/" component={Home} exact />
                         <Route path="/Cart" component={Cart} />
                         <Route path="/search" component={Search} />
+                        <Route path="/living" component={Content} />
+                        <Route path="/office" component={Content} />
+                        <Route path="/kids" component={Content} />
+                        <Route path="/kitchen" component={Content} />
+                        <Route path="/accesories" component={Content} />
                         {/* <Route component={Error} /> */}
                     </Switch>
                 </div>

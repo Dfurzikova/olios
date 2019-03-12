bd20609e86f647528ae42d7ee8c44b34883a580f
Dfurzikova <dfurzikova@yandex.ru>
2019-03-12 13:20:14 +0300

    feat: add product routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./components/Home/Home";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Cart from './components/Cart/Cart';
-import Search from './components/Search/Search'
+import Search from './components/Search/Search';
 import Hamburger from './components/Hamburger/Hamburger';
-import Content from './components/Content/Content'
+import Content from './components/Content/Content';
+import Product from './components/Product/Product'
 
 class App extends Component {
     render() {
@@ -18,11 +19,22 @@ class App extends Component {
                         <Route path="/" component={Home} exact />
                         <Route path="/Cart" component={Cart} />
                         <Route path="/search" component={Search} />
-                        <Route path="/living" component={Content} />
-                        <Route path="/office" component={Content} />
-                        <Route path="/kids" component={Content} />
-                        <Route path="/kitchen" component={Content} />
-                        <Route path="/accesories" component={Content} />
+
+                        <Route exact path="/living" component={Content} />
+                        <Route path="/living/:article" component={Product} />
+
+                        <Route exact path="/office" component={Content} />
+                        <Route path="/office/:article" component={Product} />
+
+                        <Route exact path="/kids" component={Content} />
+                        <Route path="/kids/:article" component={Product} />
+
+                        <Route exact path="/kitchen" component={Content} />
+                        <Route path="/kitchen/:article" component={Product} />
+
+                        <Route exact path="/accesories" component={Content} />
+                        <Route path="/accesories/:article" component={Product} />
+
                         {/* <Route component={Error} /> */}
                     </Switch>
                 </div>
